9be10c08319d7b794eb775ca105a100dba8105cf
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2021-10-26 10:31:40 +0530

    Removed uneccessary code

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,3 @@
-// import 'package:email_authentication_firebase_flutter/homePage.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:flutter/material.dart';
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp();
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         // This is the theme of your application.
-//         //
-//         // Try running your application with "flutter run". You'll see the
-//         // application has a blue toolbar. Then, without quitting the app, try
-//         // changing the primarySwatch below to Colors.green and then invoke
-//         // "hot reload" (press "r" in the console where you ran "flutter run",
-//         // or simply save your changes to "hot reload" in a Flutter IDE).
-//         // Notice that the counter didn't reset back to zero; the application
-//         // is not restarted.
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: const HomePage(),
-//     );
-//   }
-// }
-
 import 'package:email_authentication_firebase_flutter/home_page.dart';
 import 'package:email_authentication_firebase_flutter/login_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
